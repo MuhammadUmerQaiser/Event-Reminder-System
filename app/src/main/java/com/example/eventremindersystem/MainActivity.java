f8aa5934296823d5850dfd8b888ce3b3ad3001c0
Muhammad Umer <umerqaiser1009@gmail.com>
2023-03-08 02:09:12 +0500

    design the login/register and index ui

---
+++ app/src/main/java/com/example/eventremindersystem/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.eventremindersystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, IndexActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
